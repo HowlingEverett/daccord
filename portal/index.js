78a3ad1e59a8e33b204cb7dbe0fa3aead6a5a669
Justin Marrington <justin@marrington.net>
2015-10-02 08:33:18 +1000

    Start rendering RAML endpoints to portal

--- portal/index.js
+++ portal/index.js
@@ -1,4 +1,13 @@
+var raml = require('../raml');
 
 module.exports.apiList = function*() {
-
+  try {
+    var apiSpec = yield raml.loadApi();
+    yield this.render('api-list', apiSpec);
+  } catch(e) {
+    if (e.name === 'YAMLError') {
+      return this.body = 'Could not load RAML at specified path';
+    }
+    throw e;
+  }
 };
--- routes/portal.js
+++ routes/portal.js
@@ -1,17 +1,29 @@
+var path = require('path');
+
+var stripExtension = require('strip-extension');
+var koaHandlebars = require('koa-handlebars');
+var handlebars = require('handlebars');
 var portal = require('../portal');
 
 /**
  * Routes for the D'accord documentation portal application.
- * @param router {Object} An instance of koa-router for serving HTTP requests
+ * @param router {object} An instance of koa-router for serving HTTP requests
  */
 module.exports.routes = function(router) {
-
+  router.get('/', portal.apiList);
 };
 
 /**
  * Common middleware for the D'accord documentation portal application
- * @param  {Object} router An instance of koa-router
+ * @param {object} router An instance of koa-router
  */
 module.exports.middleware = function(router) {
-
+  router.use(koaHandlebars({
+    handlebars: handlebars,
+    defaultLayout: 'portal',
+    root: path.resolve(__dirname, '../portal'),
+    partialId: function(file) {
+      return stripExtension(file);
+    }
+  }));
 };

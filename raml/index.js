3512309e410ff62b94effd3520f203a23aa772dd
Justin Marrington <justin@marrington.net>
2015-10-02 08:33:18 +1000

    Start rendering RAML endpoints to portal

--- raml/index.js
+++ raml/index.js
@@ -1,13 +1,17 @@
 var path = require('path');
 var ramlParser = require('raml-parser');
 
+var ramlCache = {};
+
 /**
  * Parses a RAML file at a given path and returns the AST object containing the API definitions,
  * if possible.
  * @param {string} [filename] fully resolved path to a RAML definition file. If none is specified,
  * this will default to PROJECT_ROOT/api_definitions/api.raml
+ * @returns {Promise} promise instance with the
  */
-module.exports.parseApi = function(filename = '../api_definitions/api.raml') {
+module.exports.loadApi = function(filename) {
+  filename = filename || './api_definitions/api.raml';
   filename = path.resolve(filename);
   return ramlParser.loadFile(filename);
 };

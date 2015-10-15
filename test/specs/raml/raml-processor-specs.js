042fd7cead9dbdbc9fe7095f33cfacf050400d05
Justin Marrington <justin@marrington.net>
2015-10-15 15:25:08 +1000

    Finish test coverage for raml processor module.

--- test/specs/raml/raml-processor-specs.js
+++ test/specs/raml/raml-processor-specs.js
@@ -11,12 +11,16 @@ var raml = require('../../../raml');
 const DEFINITIONS_SCHEMA = require('./fixtures/definitions.json');
 const TEST_RAML = require('../portal/fixtures/books.json');
 
-test('Includes imported sub-schemas in JSON body schemas', function*(t) {
-  sinon.stub(fs, 'readFile').yieldsAsync(null, DEFINITIONS_SCHEMA);
+test('RAML Parser wrapper', function(t) {
+  sinon.stub(fs, 'readFile').yieldsAsync(null,
+    JSON.stringify(DEFINITIONS_SCHEMA));
   sinon.stub(ramlParser, 'loadFile').resolves(TEST_RAML);
 
-  let apiSpec = yield raml.loadApi();
+  t.end();
+});
 
+test('Includes imported sub-schemas in JSON body schemas', function*(t) {
+  let apiSpec = yield raml.loadApi();
   let schema = apiSpec.resources[0].resources[0].methods[1]
     .body['application/json'].schema;
   t.notOk(schema.properties.paging.$ref, '$ref key gone');
@@ -30,7 +34,23 @@ test('Includes imported sub-schemas in JSON body schemas', function*(t) {
     'record_from',
     'record_to'
   ], 'Sub-schema properties all set in-line under parent schema key');
+});
 
+test('Caches API spec in memory', function*(t) {
+  fs.readFile.reset();
+  yield raml.loadApi();
+  t.notOk(fs.readFile.called, 'Should not have called readFile');
+});
+
+test('Returns a copy of the schema each time', function*(t) {
+  let ramlA = yield raml.loadApi();
+  let ramlB = yield raml.loadApi();
+  t.notEqual(ramlA, ramlB,
+    'Load the same API twice, get a different reference');
+});
+
+test('End Parser wrapper tests', function(t) {
   ramlParser.loadFile.restore();
   fs.readFile.restore();
+  t.end();
 });
